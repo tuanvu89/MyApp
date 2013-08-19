ff75bde4aad59c8de97cf616fade0dfa0e96494f
kerofrog <ng.tuanvu89@gmail.com>
2013-08-19 15:27:02 +0700

    19/8/2013

---
+++ NearbyBank/NearbyBank/BIDViewController.h
@@ -0,0 +1,14 @@
+//
+//  BIDViewController.h
+//  NearbyBank
+//
+//  Created by kerofrog on 8/19/13.
+//  Copyright (c) 2013 kerofrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+
+@end
---
+++ NearbyBank/NearbyBank/BIDViewController.m
@@ -0,0 +1,39 @@
+//
+//  BIDViewController.m
+//  NearbyBank
+//
+//  Created by kerofrog on 8/19/13.
+//  Copyright (c) 2013 kerofrog. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
