ec738cf1d9feb9d264f3b37fe9043a10dd053a23
kerofrog <ng.tuanvu89@gmail.com>
2013-08-19 14:43:55 +0700

    19/8/2013

---
+++ NearbyBank/NearbyBank/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  NearbyBank
+//
+//  Created by kerofrog on 8/19/13.
+//  Copyright (c) 2013 kerofrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
