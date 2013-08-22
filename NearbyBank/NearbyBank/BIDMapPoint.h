6bd3d15f932a9c9806b9e3006e5f7bfc074b2697
kerofrog <ng.tuanvu89@gmail.com>
2013-08-22 14:31:47 +0700

    2h31 22/8/2013

---
+++ NearbyBank/NearbyBank/BIDMapPoint.h
@@ -0,0 +1,13 @@
+//
+//  BIDMapPoint.h
+//  NearbyBank
+//
+//  Created by kerofrog on 8/22/13.
+//  Copyright (c) 2013 kerofrog. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface BIDMapPoint : MKAnnotationView
+
+@end
